0bfdad1afa71a104d03ec21be0f4e0072b4b0eac
anasofiav04 <anasofiav04@gmail.com>
2025-10-27 21:38:21 -0600

    new changes

--- db.js
+++ db.js
@@ -3,12 +3,12 @@ dotenv.config();
 import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
-const MONGO_URI_local = "mongodb://localhost:27017";
+// const MONGO_URI_local = "mongodb://localhost:27017";
 
 export const connectDB = async () => {
   try {
     console.log(MONGO_URI);
-    await mongoose.connect(MONGO_URI_local);
+    await mongoose.connect(MONGO_URI);
     console.log("Mongo DB Connected Succesfully");
   } catch (error) {
     console.error("Mongo DB Connection Failed");
