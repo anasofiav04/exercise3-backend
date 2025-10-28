29d9910a358ad75b303a161138e8eedb86c2684f
anasofiav04 <anasofiav04@gmail.com>
2025-10-27 21:30:35 -0600

    endpoints

--- db.js
+++ db.js
@@ -1,10 +1,14 @@
+import dotenv from "dotenv";
+dotenv.config();
 import mongoose from "mongoose";
 
 const MONGO_URI = process.env.MONGO_URI || "mongodb://localhost:27017";
+const MONGO_URI_local = "mongodb://localhost:27017";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(MONGO_URI);
+    console.log(MONGO_URI);
+    await mongoose.connect(MONGO_URI_local);
     console.log("Mongo DB Connected Succesfully");
   } catch (error) {
     console.error("Mongo DB Connection Failed");
--- index.js
+++ index.js
@@ -26,9 +26,10 @@ app.get("/getAllCards", async (req, res) => {
   }
 });
 
-app.get("/getCards/id", async (req, res) => {
+app.get("/getCards/:id", async (req, res) => {
   try {
-    const card = await Card.find();
+    const { id } = req.params;
+    const card = await Card.findById(id);
     console.log(card);
     res.status(201).json(card).send("Card created succesfully");
   } catch (error) {
@@ -36,20 +37,58 @@ app.get("/getCards/id", async (req, res) => {
   }
 });
 
-app.post("/send", (req, res) => {
-  const { user, email } = req.body;
-  console.log("Datos recibidos:" + user + "" + email);
+app.delete("/deleteCard/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedCard = await Card.findByIdAndDelete(id);
 
-  res.status(200).send("Data received succesfully");
+    if (!deletedCard) {
+      return res.status(404).json({ message: "Card not found" });
+    }
+    res.status(200).json({ message: "Card deleted successfully" });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error deleting card" });
+  }
 });
 
-app.get("/hello", (req, res) => {
-  res.status(200).send("Hola mundo desde Node JS");
+app.put("/updateCard/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const updates = req.body;
+
+    const updatedCard = await Card.findByIdAndUpdate(id, updates, {
+      new: true,
+    });
+
+    if (!updatedCard) {
+      return res.status(404).json({ message: "Card not found" });
+    }
+
+    res.status(200).json({
+      message: "Card updated successfully",
+      data: updatedCard,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Error updating card" });
+  }
 });
 
-app.get("/hola", (req, res) => {
-  res.status(200).send("Hola world");
-});
+// app.post("/send", (req, res) => {
+//   const { user, email } = req.body;
+//   console.log("Datos recibidos:" + user + "" + email);
+
+//   res.status(200).send("Data received succesfully");
+// });
+
+// app.get("/hello", (req, res) => {
+//   res.status(200).send("Hola mundo desde Node JS");
+// });
+
+// app.get("/hola", (req, res) => {
+//   res.status(200).send("Hola world");
+// });
 
 app.listen(3000, () => {
   console.log("Servidor ejecutandose en https://localhost:3000");
